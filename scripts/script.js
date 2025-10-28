f4049ca0368558ded9075bca97a690d03b339a4e
Sergiy <sergiysura02@gmail.com>
2025-10-28 23:13:26 +0200

    fix yaroslava working

--- scripts/script.js
+++ scripts/script.js
@@ -23,7 +23,143 @@ function lightenColor(rgb, percent) {
     const lighten = (channel) => Math.min(255, Math.floor(channel + (255 - channel) * (percent / 100)));
     return `rgb(${lighten(r)}, ${lighten(g)}, ${lighten(b)})`;
 }
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function debounce (func, wait) {
+    let timeoutId;
+    return function (...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => func.apply(this, args), wait);
+    }
+}
+
+const debounceResize = debounce(()=>{
+    regenerateStars()
+}, 600)
+
+
+window.addEventListener('resize', ()=>{
+    debounceResize()
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+function rundom(){
+    
+}
+
+
+function randomColor() {
+    let colors = [
+        '#ef6191', 
+        '#ea5044',
+        '#f48325',
+        '#fcde4c',
+        '#5fc778',
+        '#20a786',
+        '#4896d4',
+        '#8f65a4',
+        '#98bfe6',
+        '#50edb8'
+    ]
+    let hex = Math.floor(Math.random()*colors.length);
+    let result = colors[hex];
+    return result
+}
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function regenerateStars(){
+    stars = [];
+    x = 15;
+    for (let i = 0; i < x; i++) {
+        stars.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            radius: randomInteger(3, 7),
+            vx: Math.random() * 10 - 5,
+            vy: Math.random() * 10 - 5,
+            color: randomColor(),
+        });
+    }
+}
+
+let stars = [];
+let fps = 5;
+// let x = canvas.width;
+regenerateStars()
+
+// for (let i = 0; i < x; i++) {
+//     stars.push({
+//         x: Math.random() * canvas.width,
+//         y: Math.random() * canvas.height,
+//         radius: Math.random(),
+//         vx: Math.random() * 10 - 5,
+//         vy: Math.random() * 10 - 5,
+//     });
+// }
+
+function createFireFly(posX, posY, radius, color) {
+    ctx.beginPath();
+    ctx.fillStyle = `${color}`;
+    ctx.filter = `blur(${radius}px)`;
+    ctx.arc(posX, posY, radius*2, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.filter = `blur(${radius/5}px)`;
+    ctx.fillStyle = 'white';
+    ctx.arc(posX, posY, radius, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.globalCompositeOperation="lighter"
+    let blur = 100;
+    for (let i = 0; i<stars.length; i++) {
+        let star = stars[i];
+        // ctx.fillStyle = star.color;
+        // ctx.beginPath();
+        // ctx.filter = `blur(${star.radius}px)`;
+        // ctx.arc(star.x, star.y, star.radius*2, 0, Math.PI * 2);
+        // ctx.fill();
+        // ctx.beginPath();
+        // ctx.filter = `blur(${star.radius/5}px)`;
+        // ctx.fillStyle = 'white';
+        // ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+        // ctx.fill();
+        createFireFly(star.x, star.y, star.radius, star.color)
+        
+    }
+}
+
+
+function update() {
+    for (let i = 0; i<stars.length; i++) {
+        let s = stars[i];
+        s.x += s.vx / fps;
+        s.y += s.vy / fps;
+        if (s.x >= canvas.width || s.x <= 0) {
+            s.vx = -s.vx;
+        }
+        if (s.y >= canvas.height || s.y <= 0) {
+            s.vy = -s.vy;
+        }
+    }
+}
+
+
+function tick() {
+    update();
+    draw();
+    requestAnimationFrame(tick);
+}
+
+tick();
 /*
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
