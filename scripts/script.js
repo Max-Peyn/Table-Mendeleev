a238f6f33dd1ff7d003c25d5f821d8ebbe8f87cb
Sergiy <sergiysura02@gmail.com>
2025-10-28 19:44:18 +0200

    create folder images

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,15 @@ let elements = document.querySelectorAll(".element");
 elements.forEach(element => {
     element.onclick = () => {
         selected.innerHTML = "";
-
         let cloneElement = element.cloneNode(true);
         cloneElement.className = element.className;
-
-        // Копіюємо стилі
         let computedStyle = window.getComputedStyle(element);
         let baseColor = computedStyle.backgroundColor;
-
-        // Створюємо світліший колір
-        let lightColor = lightenColor(baseColor, 40); // змінює яскравість на +40
-
-        // Задаємо радіальний градієнт
+        let lightColor = lightenColor(baseColor, 40);
         cloneElement.style.background = `radial-gradient(circle at center, ${lightColor}, ${baseColor})`;
         cloneElement.style.color = computedStyle.color;
         cloneElement.style.border = 'none';
         cloneElement.style.boxShadow = computedStyle.boxShadow;
-
         selected.appendChild(cloneElement);
     }
 });
@@ -31,3 +23,27 @@ function lightenColor(rgb, percent) {
     const lighten = (channel) => Math.min(255, Math.floor(channel + (255 - channel) * (percent / 100)));
     return `rgb(${lighten(r)}, ${lighten(g)}, ${lighten(b)})`;
 }
+
+/*
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+let centerPointX = canvas.width / 2;
+let centerPointY = canvas.height / 2;
+
+function createFireFly(posX, posY, radius, color, blur) {
+    ctx.beginPath();
+    ctx.fillStyle = `${color}`;
+    ctx.filter = `blur(${blur}px)`;
+    ctx.arc(posX, posY, radius*2, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.filter = `blur(${blur/5}px)`;
+    ctx.fillStyle = 'white';
+    ctx.arc(posX, posY, radius, 0, Math.PI * 2);
+    ctx.fill();
+}
+// createFireFly(centerPointX, centerPointY, 10, 'yellow', 10)
+createFireFly(100, 70, 15, 'yellow', 15)
+createFireFly(210, 120, 7, 'yellow', 7)
+createFireFly(150, 170, 10, 'yellow', 10)
+*/
